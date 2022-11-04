cd5fd400c6bdef132e0fac18f20363877d63ca14
Mofejesu Famuyiwa <niciepearl@gmail.com>
2022-11-04 10:22:17 +0100

    edited all, now working perfectly

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-function play (){
-    let input = document.querySelector('#input');
-    let newInput = Array.from(input.value)
-   
-    const random =[]
-    const user = newInput;
-    
+function play(){
+    const randomno =[]
     for(let i =0; i  < 6; i++ ){
-        random[i] = Math.floor(Math.random() * 11)
+        randomno[i] = Math.floor(Math.random() *101)
 }
 
-    const correctGuess = [];
+    const correctscore = [];
     let correctIndex = 0
+    let input = document.querySelector('#input').value;
+    newInputvalue = input.split(',')
+    const user = newInputvalue;
+    
 
     for(let value of user){
-        let toNum = parseInt(value)
-        if(random.includes(toNum)){
-            correctGuess[correctIndex] = value;
+        let number = parseInt(value);
+        if(randomno.includes(number)){
+            correctscore[correctIndex] = number;
             correctIndex++
         }
-    }
+    
 
-    if(correctGuess.length >=3){
-        document.getElementById('result').innerHTML = "You win!"
-    }else{
-        document.getElementById('result').innerHTML = "You lose!"
-    }
-
-    document.getElementById('random').innerHTML = random
-    document.getElementById('predictednum').innerHTML = correctGuess 
+    if (correctscore.length >=3 && number >=1 || number<= 100 && number.length == 3){
+        document.getElementById('result').innerHTML = "You win!";
+        document.getElementById('random').value = randomno;
+        document.getElementById('predictednum').value = correctscore;
 }
+
+    else if(correctscore.length < 3 && number >=1 || number <=100 && number.length == 3){
+        document.getElementById('result').innerHTML = "You lose!";
+        document.getElementById('random').value = randomno;
+        document.getElementById('predictednum').value = correctscore;
+}
+
+    else{
+        document.getElementById('result').innerHTML = "invalid!";
+        document.getElementById('random').value = '';
+        document.getElementById('predictednum').value = correctscore;
+}
+    }
+}
+
+
